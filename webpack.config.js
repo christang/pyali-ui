bbc606b0f06b04f362f428d694deeaa7f9492a11
Chris Tang <chris.l.tang@gmail.com>
2017-02-20 08:46:47 -0500

    Init react project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.join(__dirname, '/app'),
+  entry: [
+    'webpack/hot/only-dev-server',   // <-- Tries HMR but DOESN'T reload browser upon errors
+    // 'webpack/hot/dev-server',   // <-- (Default) Tries HMR AND reloads the browser upon errors
+    './Main.js',
+  ],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),   // <-- HMR-plugin
+  ],
+  module: {
+    loaders: [{
+      test: /.jsx?$/,
+      loaders: [
+        'react-hot-loader',  // <-- react-hot-loader
+        'babel-loader?presets[]=es2015&presets[]=react',  // <-- mv query into loader string
+      ],
+      exclude: /node_modules/,
+    }, {
+      test: /\.css$/,
+      loader: 'style!css',  // <-- style-loader
+    }],
+  },
+};
