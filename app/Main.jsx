d77ae6493fb31d13a671fc849b7d94bf681e5d1f
Chris Tang <chris.l.tang@gmail.com>
2017-02-25 19:13:27 -0500

    update alignment ux
    replaces select child loop with map-func loop showing all children at once

--- app/Main.jsx
+++ app/Main.jsx
@@ -18,85 +18,72 @@ const cleanMsg = msg => msg
   .replace(/\s+$/, '')
   .replace(/ /g, '');
 
-// like Array.indexOf, except it finds indices of all occurrences
-const indicesOf = (lambda, arr) => arr
-  .map((c, i) => (lambda(c, i) ? i : -1))
-  .filter(n => n >= 0);
-
-// char indices of \n in a string, as well as those before and after its first and last chars
-const indicesOfSplits = msg => [-1, ...indicesOf(c => c === '\n', msg.split('')), msg.length + 1];
-
-// given selected char index of a string, find which \n-sep substring it's found in
-const locIndexArray = (si, arr) => indicesOf((c, i) => arr[i - 1] < si && si <= arr[i], arr)[0] - 1;
+const cleanSeqs = seqs => seqs
+  .map((s, i) => [i, s])
+  .filter(tup => tup[1] && tup[1].length);
 
 class MainContainer extends React.Component {
   constructor(props) {
     super(props);
     this.handleChangeRef = this.handleChangeRef.bind(this);
     this.handleChangeAli = this.handleChangeAli.bind(this);
-    this.handleSelect = this.handleSelect.bind(this);
-    this.state = { query: { ref: [], seqs: {} }, refMsg: '', splits: [], loc: -1 };
-  }
-  handleSelect(e) {
-    const s1 = locIndexArray(e.target.selectionStart, this.state.splits);
-    const s2 = locIndexArray(e.target.selectionEnd, this.state.splits);
-    if (s1 === s2) {
-      this.setState({ loc: s1 });
-    } else {
-      this.setState({ loc: -1 });
-    }
-    // console.log(`${s1} ${s2}`);
+    this.handleMessage = this.handleMessage.bind(this);
+    this.state = { ref: [], seqs: [], value: '', err: '' };
   }
   handleChangeRef(e) {
-    const refMsg = cleanMsg(e.target.value);
-    const splits = refMsg ? indicesOfSplits(refMsg) : [];
-    const query = { ref: refMsg.split(/\n/), seqs: {} };
-    this.setState({ query, refMsg, splits });
-    // console.log(splits);
+    const ref = cleanMsg(e.target.value)
+      .split(/\n/).filter(_ => _);
+    this.setState({ ref, seqs: this.state.seqs.map(() => []), value: '', err: '' });
   }
-  handleChangeAli(e) {
+  handleChangeAli(e, i) {
     const aliMsg = cleanMsg(e.target.value);
-    const query = Object.assign({}, this.state.query);
-    this.state.query.seqs[this.state.loc] = aliMsg;
-    this.setState({ query, loc: -1 });
+    const seqs = this.state.seqs.slice(0);
+    seqs[i] = aliMsg.split(/\n/);
+    this.setState({ seqs, value: '', err: '' });
+    this.handleMessage(cleanSeqs(seqs));
   }
-  __handleChange(e) {
-    function parseIn(msg) {
-      const msgParts = msg
-        .replace(/^\s+/, '')
-        .replace(/\s+$/, '')
-        .replace(/ /g, '')
-        .split(/\n\n+/);
-      const ref = msgParts
-        .shift()
-        .split(/\n/);
-      const seqs = msgParts
-        .map((msgAli) => {
-          const aliParts = msgAli.split(/\n/);
-          return [parseInt(aliParts.shift(), 10), aliParts];
-        });
-
-      const json = { ref, seqs };
-      return json;
-    }
+  handleMessage(seqs) {
+    const ref = this.state.ref;
+    const msg = { ref, seqs };
     fetch(apiURL, {
       method: 'POST',
-      body: JSON.stringify(parseIn(e.target.value)),
+      body: JSON.stringify(msg),
     }).then((response) => {
       response.json().then((json) => {
         this.setState({ value: json.result, err: '' });
+      }).catch((err) => {
+        this.setState({ err });
       });
-    }).catch((error) => {
-      this.setState({ err: error });
     });
   }
   render() {
+    const notEmpty = ali => !!ali && ali.length > 0;
     return (
       <Grid>
-        <AlignmentContainer onSelect={this.handleSelect} onChange={this.handleChangeRef} value={this.state.refMsg} label="XX" placeholder="YY" />
-        { this.state.loc >= 0 ? <AlignmentContainer label={`bind ${this.state.loc}`} placeholder="AA" onChange={this.handleChangeAli} /> : '' }
-        <AlignmentContainer label="Combined Alignment" placeholder="ZZ" value={this.state.value} />
-        { this.state.err ? <Alert bstyle="warning"><strong>Oh no!</strong>The server said: {}</Alert> : '' }
+        <AlignmentContainer
+          alignment={notEmpty(this.state.ref)}
+          label="Reference Alignment"
+          placeholder="Paste your reference alignment here"
+          onChange={this.handleChangeRef}
+        />
+        { this.state.ref.map((s, i) => (
+          <AlignmentContainer
+            alignment={notEmpty(this.state.seqs[i])}
+            key={i}
+            label="Child alignment for ..."
+            placeholder={this.state.ref[i].replace(/-/, '')}
+            onChange={e => this.handleChangeAli(e, i)}
+          />)) }
+        <AlignmentContainer
+          alignment={notEmpty(this.state.value)}
+          label="Merged Alignment"
+          value={this.state.value}
+        />
+        { this.state.err ? (
+          <Alert bsStyle="warning">
+            <strong>Oh no! </strong>
+            {`The server said: ${JSON.stringify(this.state.err)}`}
+          </Alert>) : '' }
       </Grid>
     );
   }
@@ -105,28 +92,33 @@ class MainContainer extends React.Component {
 const AlignmentContainer = props => (
   <Row className="show-grid"><Col md={12}>
     <FormGroup controlId="formControlsTextarea">
-      <ControlLabel>{props.label}</ControlLabel>
+      <ControlLabel
+        style={{ fontWeight: props.label.startsWith('Child') ? 'unset' : '' }}
+      >
+        {props.label}
+      </ControlLabel>
       <textarea
-        className="form-control" style={{ fontFamily: 'monospace', whiteSpace: 'pre' }}
-        placeholder={props.placeholder} value={props.value}
-        onChange={props.onChange} onSelect={props.onSelect}
+        className="form-control"
+        placeholder={props.placeholder} onChange={props.onChange}
+        style={{ fontFamily: props.alignment ? 'monospace' : '', whiteSpace: 'pre' }}
+        value={props.value}
       />
     </FormGroup>
   </Col></Row>
 );
 
 AlignmentContainer.propTypes = {
+  alignment: React.PropTypes.bool,
   label: React.PropTypes.string,
   onChange: React.PropTypes.func,
-  onSelect: React.PropTypes.func,
   placeholder: React.PropTypes.string,
   value: React.PropTypes.string,
 };
 
 function AppNavbar() {
   function handleSelect(eventKey) {
-    const helpMsg = "Paste this into the 'Reference Alignment' area:\n\nabcde- \n-bcdef \n\n0\nab-cde\n-bbcd-\n\n1\nbcdef\n-cde-\n";
-    const aboutMsg = "Just a li'l informatics tool © 2017 // chris@entangible";
+    const helpMsg = "Paste this into the 'Reference Alignment' area:\n\nabcde- \n-bcdef\n\nPaste this into 'Child alignment for ... [abcde]'\n\nab-cde\n-bbcd-\n\nPaste this into 'Child alignment for ... [bcdef]'\n\nbcdef\n-cde-\n";
+    const aboutMsg = "An alignment informatics tool '\n\n © 2017 // chris@entangible";
     alert(`${[undefined, helpMsg, aboutMsg][eventKey]}`);
   }
   return (
